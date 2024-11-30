73fecd36fc4c9c8c00cf80ec881c14510f52fd5f
mrcurlybrown <mrcurlybrown@gmail.com>
2024-11-30 22:35:29 +0100

    Abort process by PID

--- pkg/abort/abort.go
+++ pkg/abort/abort.go
@@ -1,5 +1,42 @@
 package abort
 
-// TODO Implement functionality to abort process by ID.
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+func ByID(pid int) int {
+	// Find process by PID.
+	p, err := os.FindProcess(pid)
+	if err != nil {
+		fmt.Println("Error: Process could not be found. ", p)
+	}
+
+	fmt.Println("Process INFO:", p)
+
+	// Check if process actually exists.
+	isProcessRunning(p)
+
+	// Kill process
+	p.Kill()
+
+	// Check if process has been killed or it still runs.
+	status := isProcessRunning(p)
+
+	// Return status code & error if any
+	if status != nil {
+		return 1
+	}
+	return 0
+}
+
+func isProcessRunning(p *os.Process) error {
+	err := p.Signal(syscall.Signal(0))
+	if err != nil {
+		fmt.Println("ERROR: Not found process:", err)
+	}
+	return err
+}
 
 // TODO Implement functionality to abort process by Name.
--- tests/abort_test.go
+++
@@ -1,7 +0,0 @@
-package tests
-
-// TODO Import abort package.
-
-// TODO Write test to kill process by ID.
-
-// TODO Write test to kill process by Name.
